d7c7e9ebc73dc0458249d607d19a286dca7af8b0
Adrian Labbé <adrilabbelol@gmail.com>
2018-10-06 19:21:27 -0300

    Update project's hierarchy

---
+++ LibTerm/Helpers/CommandHelp.swift
@@ -0,0 +1,59 @@
+//
+//  CommandHelp.swift
+//  LibTerm
+//
+//  Created by Adrian Labbe on 10/6/18.
+//  Copyright © 2018 Adrian Labbe. All rights reserved.
+//
+
+import ios_system
+
+/// A structure representing a command for helping the user.
+struct CommandHelp: Equatable {
+    
+    /// The command name.
+    var commandName: String
+    
+    /// The argument type the commands supports. `.none`, `.file` or `.directory`.
+    var commandInput: CompletionType {
+        didSet {
+            if commandInput == .history || commandInput == .command {
+                fatalError("History and command aren't supported for a command input yet.")
+            }
+        }
+    }
+    
+    /// Returns flags supported by the command.
+    var flags: [String] {
+        var flags_ = [String]()
+        for flag in getoptString(commandName) {
+            if flag != ":" {
+                flags_.append("-\(flag)")
+            }
+        }
+        return flags_
+    }
+    
+    /// A structure representing completions type available for a command.
+    enum CompletionType {
+        
+        /// No completion
+        case none
+        
+        /// Show the history.
+        case history
+        
+        /// Show commands matching by the user's input.
+        case command
+        
+        /// Show files and directories in the current directory.
+        case file
+        
+        /// Show directories in the current directory.
+        case directory
+    }
+    
+    static func == (lhs: CommandHelp, rhs: CommandHelp) -> Bool {
+        return (lhs.commandName == rhs.commandName)
+    }
+}
