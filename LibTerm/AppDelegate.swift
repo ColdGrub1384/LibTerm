21e3052b3e2da2b70f8c92d31a815b792dfeb81f
Adrian Labbé <adrilabbelol@gmail.com>
2018-12-15 21:23:55 -0300

    Fix Python

--- LibTerm/AppDelegate.swift
+++ LibTerm/AppDelegate.swift
@@ -27,7 +27,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         replaceCommand("pbcopy", "pbcopy", true)
         replaceCommand("pbpaste", "pbpaste", true)
-        replaceCommand("python", "python3_main", true)
         
         // Python
         putenv("PYTHONHOME=\(Bundle.main.bundlePath)".cValue)
