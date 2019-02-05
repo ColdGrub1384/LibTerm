31307b1df79290274137261edce422f32e8ceade
ColdGrub1384 <adrilabbelol@gmail.com>
2019-02-05 12:14:49 -0300

    Fix site-packages

--- LibTerm/LibShell.swift
+++ LibTerm/LibShell.swift
@@ -433,7 +433,7 @@ open class LibShell {
         if version == .v2_7 {
             putenv("PYTHONPATH=\(py2SitePackages):\(py2Path):\(bundledSitePackages)".cValue)
         } else if version == .v3_7 {
-            putenv("PYTHONPATH=\(py3SitePackages):\(py3Path):\(bundledSitePackages3):".cValue)
+            putenv("PYTHONPATH=\(py3SitePackages):\(py3Path):\(bundledSitePackages):\(bundledSitePackages3)".cValue)
         }
     }
     
