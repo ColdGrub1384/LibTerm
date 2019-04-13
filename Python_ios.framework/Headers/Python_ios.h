752d55476b3c47d56f4a74fe7525537bb64bfa55
ColdGrub1384 <adrilabbelol@gmail.com>
2019-04-13 13:48:09 -0400

    Add `_multiprocessing` module to Python 2

---
+++ Python_ios.framework/Headers/Python_ios.h
@@ -0,0 +1,19 @@
+//
+//  Python_ios.h
+//  Python_ios
+//
+//  Created by Nicolas Holzschuch on 04/07/2017.
+//  Copyright © 2017 Nicolas Holzschuch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Python_ios.
+FOUNDATION_EXPORT double Python_iosVersionNumber;
+
+//! Project version string for Python_ios.
+FOUNDATION_EXPORT const unsigned char Python_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Python_ios/PublicHeader.h>
+extern int python_main(int argc, char **argv);
+
