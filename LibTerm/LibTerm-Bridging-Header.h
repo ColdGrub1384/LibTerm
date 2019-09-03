7c2d75f73a026e0d70d1cfbf6b12e482b649ddd9
Adrian Labbé <adrilabbelol@gmail.com>
2019-09-02 22:59:22 -0400

    Use Python 3 from ios_system

--- LibTerm/LibTerm-Bridging-Header.h
+++ LibTerm/LibTerm-Bridging-Header.h
@@ -1,7 +1,3 @@
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
-
-#if !(TARGET_IPHONE_SIMULATOR)
-int _Py_UnixMain(int argc, char **argv);
-#endif
